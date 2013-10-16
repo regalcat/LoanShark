75cedf92e421923c23428dd3189d887701e4b871
clwehrle <clwehrle@iastate.edu>
2013-10-16 14:45:01 -0500

    First Skeleton Code

---
+++ S_E_319/S_E_319/Model/BookItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_E_319
+{
+    class BookItem : ItemBaseClass
+    {
+        #region Fields and Autoproperties
+
+        public string Author { get; private set; }
+        public int ISBN { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public BookItem(string name, string author, int isbn, string owner, string category)
+        {
+            Name = name;
+            Author = author;
+            ISBN = isbn;
+            Owner = owner;
+            Borrower = "N/A";
+            Category = category;
+        }
+
+        #endregion
+    }
+}
