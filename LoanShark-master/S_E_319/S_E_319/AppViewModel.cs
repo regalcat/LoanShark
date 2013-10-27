95effe975d42a0e048c539c55a285c98b7f83eda
regalcat <regalcat17@gmail.com>
2013-10-27 10:56:19 -0500

    I'mma testin' the github

---
+++ LoanShark-master/S_E_319/S_E_319/AppViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_E_319
+{
+    class AppViewModel
+    {
+        #region Fields and Autoproperties
+
+        public MenuBarViewModel MenuBar { get; private set; }
+        public ToolBarViewModel ToolBar { get; private set; }
+        public SidePanelViewModel SidePanel { get; private set; }
+        public MainBrowserViewModelBaseClass MainBrowser { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public AppViewModel()
+        {
+            MainBrowserViewModelBaseClass.GenerateList();
+            MenuBar = new MenuBarViewModel();
+            ToolBar = new ToolBarViewModel();
+            SidePanel = new SidePanelViewModel();
+            MainBrowser = new MainBrowserTileViewModel();
+        }
+
+        #endregion
+    }
+}
---
+++ LoanShark-master/S_E_319/S_E_319/ViewModel/MainBrowserViewModelBaseClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_E_319
+{
+    abstract class MainBrowserViewModelBaseClass : ViewModelBase
+    {
+        #region Fields and Autoproperties
+        private static ObservableCollection<ItemBaseClass> _items;
+        #endregion
+
+
+        #region Properties
+
+        public ObservableCollection<ItemBaseClass> Items
+        {
+            get { return _items; }
+            protected set { _items = value; OnPropertyChanged("Items"); }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public static void GenerateList()
+        {
+            _items = new ObservableCollection<ItemBaseClass>();
+            _items.Add(new BookItem("Harry Potter", "J.K. Rowling", 929292, "Caleb", "Fantasy"));
+            _items.Add(new BookItem("The Bible", "Our God Almighty", 1, "Caleb", "Fantasy"));
+            _items.Add(new MiscItem("Nic-nak", "Caleb", "Misc"));
+        }
+
+        #endregion
+
+    }
+}
